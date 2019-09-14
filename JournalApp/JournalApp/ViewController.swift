83d02cf5029a44d1016318ba6dc14657d1999495
Rajat Mittal <rajat0502@gmail.com>
2019-09-14 15:35:58 -0400

    initial commit

---
+++ JournalApp/JournalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JournalApp
+//
+//  Created by Rajat Mittal on 9/14/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
