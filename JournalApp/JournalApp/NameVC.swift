1118527f505e0266e7e9fed3b6e3f0dd63e11cb8
Rajat Mittal <rajat0502@gmail.com>
2019-09-15 00:29:15 -0400

    adding realm in named

--- JournalApp/JournalApp/NameVC.swift
+++ JournalApp/JournalApp/NameVC.swift
@@ -7,16 +7,30 @@
 
 import Foundation
 import UIKit
+import RealmSwift
 
 class NameVC: UIViewController {
     
     @IBOutlet weak var nameField: UITextField!
+    private var user: User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print(Date.init())
+        let realm = try! Realm()
+        
+        if realm.objects(User.self).count == 0 {
+            try! realm.write {
+                let newUser = User()
+                realm.add(newUser)
+                user = newUser
+            }
+        } else {
+            user = realm.objects(User.self)[0]
+            performSegue(withIdentifier: "segue1", sender: nameField.text)
+        }
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
         
         let clickViewGesture = UITapGestureRecognizer(target: self, action:  #selector (self.clickView (_:)))
         self.view.addGestureRecognizer(clickViewGesture)
@@ -28,17 +42,20 @@ class NameVC: UIViewController {
             nameField.resignFirstResponder()
         }
     }
+    
     @IBAction func nameSubmittedViaKeyboard(_ sender: Any) {
         if nameField.isFirstResponder {
             nameField.resignFirstResponder()
         }
         if (nameField.text != "") {
+            user.name = nameField.text!
             performSegue(withIdentifier: "segue1", sender: nameField.text)
         } 
     }
     
     @IBAction func nameSubmitted(_ sender: Any) {
         if (nameField.text != "") {
+            user.name = nameField.text!
             performSegue(withIdentifier: "segue1", sender: nameField.text)
         } 
     }
