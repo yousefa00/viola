70297bb77e40ad295e71df931c48291adcce2ad0
Kaahan <kaahan15@gmail.com>
2019-09-14 22:59:44 -0500

    swiping from second two screens finally fixed!

---
+++ JournalApp/SegueFromRight.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  JournalApp
+//
+//  Created by Kaahan Motwani on 9/14/19.
+//
+
+import Foundation
+import UIKit
+class SegueFromRight: UIStoryboardSegue {
+    override func perform() {
+        let src = self.source
+        let dst = self.destination
+        
+        src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
+        dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
+        
+        UIView.animate(withDuration: 0.25,
+                       delay: 0.0,
+                       options: .curveEaseInOut,
+                       animations: {
+                        dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+        },
+                       completion: { finished in
+                        src.present(dst, animated: false, completion: nil)
+        }
+        )
+    }
+}
