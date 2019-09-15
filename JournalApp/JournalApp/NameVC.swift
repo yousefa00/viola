8816961a9734e2dfacf3fb25378eb03d498a478d
Yousef Ahmed <yousefahmed0066@gmail.com>
2019-09-15 06:11:14 -0400

    Revert "Merge branch 'master' of https://github.com/yousefa00/viola"
    This reverts commit 45c3e5df44a20f3ce4c273df8dd9dc4a320d2abc, reversing
    changes made to 3c58195699a596e72b6cc3c7d5edecc6b524023c.

--- JournalApp/JournalApp/NameVC.swift
+++ JournalApp/JournalApp/NameVC.swift
@@ -7,15 +7,17 @@
 
 import Foundation
 import UIKit
+import RealmSwift
 
 class NameVC: UIViewController {
-
+    
     @IBOutlet weak var nameField: UITextField!
-
+    private var user: User!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        
         let realm = try! Realm()
         print(realm.objects(User.self).count)
         if realm.objects(User.self).count == 0 {
@@ -29,24 +31,25 @@ class NameVC: UIViewController {
             print("running")
         }
         print(Realm.Configuration.defaultConfiguration.fileURL!)
-
+        
         let clickViewGesture = UITapGestureRecognizer(target: self, action:  #selector (self.clickView (_:)))
         self.view.addGestureRecognizer(clickViewGesture)
-
+        
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if (user.name != "") {
             performSegue(withIdentifier: "segue1", sender: nameField.text)
         }
     }
-
+    
     @objc func clickView(_ sender:UITapGestureRecognizer){
         if nameField.isFirstResponder {
             nameField.resignFirstResponder()
         }
     }
+    
     @IBAction func nameSubmittedViaKeyboard(_ sender: Any) {
         if nameField.isFirstResponder {
             nameField.resignFirstResponder()
@@ -57,9 +60,9 @@ class NameVC: UIViewController {
                 user.name = nameField.text!
             }
             performSegue(withIdentifier: "segue1", sender: nameField.text)
-        }
+        } 
     }
-
+    
     @IBAction func nameSubmitted(_ sender: Any) {
         if (nameField.text != "") {
             let realm = try! Realm()
@@ -67,6 +70,6 @@ class NameVC: UIViewController {
                 user.name = nameField.text!
             }
             performSegue(withIdentifier: "segue1", sender: nameField.text)
-        }
+        } 
     }
 }
