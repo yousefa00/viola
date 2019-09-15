dd21c72b1b8589322dc2e086677cd5df41e5e843
Rajat Mittal <rajat0502@gmail.com>
2019-09-14 22:31:01 -0400

    namevc class added

---
+++ JournalApp/JournalApp/NameVC.swift
@@ -0,0 +1,24 @@
+//
+//  NameVC.swift
+//  JournalApp
+//
+//  Created by Rajat Mittal on 9/14/19.
+//
+
+import Foundation
+import UIKit
+
+class NameVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print(Date.init())
+        
+    }
+    
+    @IBAction func nameSubmitted(_ sender: Any) {
+        
+    }
+}
