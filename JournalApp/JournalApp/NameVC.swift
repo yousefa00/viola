6891b99f75d1813ebbb11ffedb3a98e21c812f7f
Kaahan <kaahan15@gmail.com>
2019-09-15 05:36:23 -0400

    Revert "Merge branch 'master' of https://github.com/yousefa00/viola"
    This reverts commit 6edfa76c75d588e3ea34411c5bbcae9dcf6e23e5, reversing
    changes made to f59a60c3cce7882b59cbb996427fa8ddd6e4e0dd.

--- JournalApp/JournalApp/NameVC.swift
+++ JournalApp/JournalApp/NameVC.swift
@@ -7,30 +7,16 @@
 
 import Foundation
 import UIKit
-import RealmSwift
 
 class NameVC: UIViewController {
     
     @IBOutlet weak var nameField: UITextField!
-    private var user: User!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let realm = try! Realm()
-        
-        if realm.objects(User.self).count == 0 {
-            try! realm.write {
-                let newUser = User()
-                realm.add(newUser)
-                user = newUser
-            }
-        } else {
-            user = realm.objects(User.self)[0]
-            performSegue(withIdentifier: "segue1", sender: nameField.text)
-        }
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        print(Date.init())
         
         let clickViewGesture = UITapGestureRecognizer(target: self, action:  #selector (self.clickView (_:)))
         self.view.addGestureRecognizer(clickViewGesture)
@@ -42,20 +28,17 @@ class NameVC: UIViewController {
             nameField.resignFirstResponder()
         }
     }
-    
     @IBAction func nameSubmittedViaKeyboard(_ sender: Any) {
         if nameField.isFirstResponder {
             nameField.resignFirstResponder()
         }
         if (nameField.text != "") {
-            user.name = nameField.text!
             performSegue(withIdentifier: "segue1", sender: nameField.text)
         } 
     }
     
     @IBAction func nameSubmitted(_ sender: Any) {
         if (nameField.text != "") {
-            user.name = nameField.text!
             performSegue(withIdentifier: "segue1", sender: nameField.text)
         } 
     }
